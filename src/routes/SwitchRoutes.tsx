906153ed1e9107f6d0bb3141be10f57586b22fda
timkinsman <tkinsm@gmail.com>
2022-09-25 21:00:28 +0930

    index exports

--- src/routes/SwitchRoutes.tsx
+++ src/routes/SwitchRoutes.tsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { NotFound } from '../views/NotFound/NotFound';
+import { NotFound } from '../views/NotFound';
 import routes from './Routes';
 
 const SwitchRoutes = () => (
